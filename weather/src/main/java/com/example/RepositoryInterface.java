b0747b46d01a6820e09389d6a031c147528d14eb
Sandra Kjellström <sandrakjellstrom@hotmail.com>
2017-03-29 14:59:23 +0200

    Databaselogger

---
+++ weather/src/main/java/com/example/RepositoryInterface.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by Administrator on 2017-03-29.
+ */
+public interface RepositoryInterface {
+
+    void addLog(String Country, String City) throws Exception;
+}

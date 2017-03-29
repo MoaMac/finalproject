1bca160a28110ec4e1a1076d96a6dc820d9d3663
MoaMac <mackegard@gmail.com>
2017-03-29 09:45:12 +0200

    Ŕemoved files testkod

--- testkod/code.js
+++
@@ -1,8 +0,0 @@
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=30ae9cbe5da2955545ae212e144318e2&units=metric', true);
-xhr.send();
-
-console.log(xhr.status);
-console.log(xhr.statusText);
-console.log(xhr.responseText);
--- testkod/weather.js
+++
@@ -1,3 +0,0 @@
-$('document').ready(function setup(){
-  loadJSON('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=30ae9cbe5da2955545ae212e144318e2&units=metric', gotData);
-});

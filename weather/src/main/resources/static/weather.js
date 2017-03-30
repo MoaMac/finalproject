4bb18837924b1a534be14d1482a903fb7ad44999
Leila Svantro <leila.svantro@gmail.com>
2017-03-30 10:53:56 +0200

    Added new functions handling weather data and implemented geo location in the working project

--- weather/src/main/resources/static/weather.js
+++ weather/src/main/resources/static/weather.js
@@ -1,4 +1,6 @@
+/*
 setTimeout(function loadPage() {
+    console.log("################## RUNNING: setTimeout(function loadPage() { [...] ");
     var xhttp = new XMLHttpRequest();
     var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + currentPosition.lat + "&lon=" + currentPosition.lng + "&APPID=30ae9cbe5da2955545ae212e144318e2&units=metric";
     console.log(url);
@@ -30,7 +32,68 @@ setTimeout(function loadPage() {
     console.log(currentPosition);
     console.log("testing testing");
 }, 50);
+*/
 
+function loadStaticDestiniation(city) {
+    console.log("*** City: " + city);
+    switch(city) {
+        case "Tokyo":
+            loadLocation(35.703129, 139.729015);
+            break;
+        case "Svalbard":
+            loadLocation(77.553604, 23.670272);
+            break;
+        case "Alaska":
+            loadLocation(61.2180556, 149.9002778);
+            break;
+        case "London":
+            loadLocation(51.5073509, -0.1277583);
+            break;
+        case "Barcelona":
+            loadLocation(41.3850639, 2.1734035);
+            break;
+        case "California":
+            loadLocation(36.778261, -119.4179324);
+            break;
+        default:
+            alert("The city \"" + city + "\" is not pre-set.");
+            break;
+    }
+}
+
+function loadLocation(lat, lon) {
+    console.log("lat: ", lat, ", lon: ", lon);
+    var xhttp = new XMLHttpRequest();
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=30ae9cbe5da2955545ae212e144318e2&units=metric";
+    console.log("url: ", url);
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+    var data = JSON.parse(xhttp.responseText)
+    var temp = data.main.temp;
+    var clouds = data.clouds.all;
+    var wind = data.wind.speed;
+    var country = data.sys.country;
+    var city = data.name;
+
+    console.log("country / city: ", country + " / " + city);
+    xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/Log/" + country + "/" + city, false);
+    xhttp.send();
+
+    console.log("temp: ", temp);
+    console.log("clouds: ", clouds);
+    console.log("data: ", data);
+    document.getElementById("temp").innerHTML = temp;
+    document.getElementById("clouds").innerHTML = clouds;
+    document.getElementById("wind").innerHTML = wind;
+
+    //console.log("testing testing");
+    console.log("currentPosition: ", currentPosition);
+    //console.log("testing testing");
+}
+
+/*
 function loadTokyo() {
     var xhttp = new XMLHttpRequest();
     var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + 35.703129 + "&lon=" + 139.729015 + "&APPID=30ae9cbe5da2955545ae212e144318e2&units=metric";
@@ -216,3 +279,4 @@ function loadCalifornia() {
     console.log(currentPosition);
     console.log("testing testing");
 }
+*/

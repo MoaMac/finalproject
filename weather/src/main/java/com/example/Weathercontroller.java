9fc0457d43208e309da0bde5635a5f10b2701e93
Leila Svantro <leila.svantro@gmail.com>
2017-03-30 10:18:34 +0200

    Added logger to Spring

--- weather/src/main/java/com/example/Weathercontroller.java
+++ weather/src/main/java/com/example/Weathercontroller.java
@@ -1,17 +1,16 @@
 package com.example;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class Weathercontroller {
+    private static final Logger logger = Logger.getLogger(Weathercontroller.class.getName());
 
     @Autowired
     RepositoryInterface repository;
@@ -20,6 +19,6 @@ public class Weathercontroller {
     public void addPos(@PathVariable String country,@PathVariable String city) throws Exception {
         System.out.println(city + country);
         repository.addLog(country, city);
-
+        //logger.log(Level.INFO, "addPos");
     }
 }
